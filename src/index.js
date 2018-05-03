9a30bbe3448845215ec8436749f43ed421c3935c
RobinMarechal <robin.marechal-2@etu.univ-tours.fr>
2018-05-03 14:36:03 +0200

    Test config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Model from './Model';
 import {REST_CONFIG} from './config';
+import QueryBuilder from './QueryBuilder';
 
 export {
     Model,
-    REST_CONFIG
+    REST_CONFIG,
+    QueryBuilder
 };
