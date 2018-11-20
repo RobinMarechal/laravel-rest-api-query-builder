c54186de1a37525b297555c8fc5460959e78552a
RobinMarechal <robin.marechal-2@etu.univ-tours.fr>
2018-11-20 23:23:29 +0100

    Cleanup

--- src/Model.js
+++ src/Model.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 import {QUERY_AWAIT_SINGLE} from './QueryBuilder';
 import {UnimplementedException} from 'bunch-of-exceptions';
-import Query from "./Query";
+import Query from './Query';
 
 export default class Model {
-    constructor() {
+    constructor () {
         this.selfValidate();
         this.type = this.getNamespace();
 
@@ -18,28 +18,28 @@ export default class Model {
 
     // override
 
-    getFields() {
-        throw UnimplementedException("Method getFields() must be implemented in every child class.");
+    getBaseUrl () {
+        throw UnimplementedException('Method getBaseUrl() must be implemented.');
     }
 
-    getDates() {
+    getFields () {
+        throw UnimplementedException('Method getFields() must be implemented.');
+    }
+
+    getNamespace () {
+        throw UnimplementedException('Method getNamespace() must be implemented.');
+    }
+
+    getDates () {
         return {};
     }
 
-    getRelations() {
+    getRelations () {
         return {};
     }
 
-    computed() {
+    computed () {
         return {};
-    }
-
-    getNamespace() {
-        throw UnimplementedException("Method getNamespace() must be implemented in every child class.");
-    }
-
-    getBaseUrl() {
-        return this.REST_CONFIG.base_url;
     }
 
     /**
@@ -47,7 +47,7 @@ export default class Model {
      * @param fetchConfig the config
      * @returns the changed fetch config
      */
-    beforeFetch(fetchConfig) {
+    beforeFetch (fetchConfig) {
         return fetchConfig;
     }
 
@@ -56,11 +56,11 @@ export default class Model {
      * @param fetchResponse the fetch response
      * @returns the changed response
      */
-    afterFetch(fetchResponse) {
+    afterFetch (fetchResponse) {
         return fetchResponse;
     }
 
-    toJson() {
+    toJson () {
         const json = {};
 
         const fields = this.getFields();
@@ -72,18 +72,15 @@ export default class Model {
         for (const prop in this) {
             if (fields.includes(prop)) {
                 json[prop] = this[prop];
-            }
-            else if (datesName.includes(prop)) {
+            } else if (datesName.includes(prop)) {
                 json[prop] = this[prop].toString();
                 if (json[prop].includes('T')) {
                     json[prop] = json[prop].replace('T', ' ').replace('Z', '');
                 }
-            }
-            else if (relationsName.includes(prop)) {
+            } else if (relationsName.includes(prop)) {
                 if (relations[prop].list) {
                     json[prop] = this[prop].map((one) => one.toJson());
-                }
-                else {
+                } else {
                     json[prop] = this[prop].toJson();
                 }
             }
@@ -92,13 +89,12 @@ export default class Model {
         return json;
     }
 
-
     /**
      * Force Lazy loading of a relation to the model and returns the loaded relation
      * @param {string} relation the relation to load and to retrieve
      * @returns {Promise<*>} the loaded relation
      */
-    async forceLazyLoadAndGet(relation) {
+    async forceLazyLoadAndGet (relation) {
         await this.forceLazyLoad(relation);
         return this[relation];
     }
@@ -108,7 +104,7 @@ export default class Model {
      * @param {string} relation the relation to load and to retrieve
      * @returns {Promise<*>} the loaded relation
      */
-    async lazyLoadAndGet(relation) {
+    async lazyLoadAndGet (relation) {
         await this.lazyLoad(relation);
         return this[relation];
     }
@@ -121,7 +117,7 @@ export default class Model {
      * @param {...string} relations a list of relations to load. The function doesn't reload an already loaded relation.
      * @returns {Promise<*>} formatted loaded relation if single, otherwise returns this.
      */
-    async lazyLoad(...relations) {
+    async lazyLoad (...relations) {
         relations = this.filterLoadedRelations(...relations);
         return this.forceLazyLoad(...relations);
     }
@@ -134,7 +130,7 @@ export default class Model {
      * @param {...string} relations a list of relations to load.
      * @returns {Promise<*>} this.
      */
-    async forceLazyLoad(...relations) {
+    async forceLazyLoad (...relations) {
         if (relations.length > 0) {
             try {
                 const newThis = await Query.model(this).with(...relations).find(this.id);
@@ -159,7 +155,7 @@ export default class Model {
      * @param {string} relations the model's relations
      * @returns {array} the filtered relations (that haven't been loaded yet)
      */
-    filterLoadedRelations(...relations) {
+    filterLoadedRelations (...relations) {
         const arr = [];
         for (const r of relations) {
             if (!this.relations[r].loaded) {
@@ -172,8 +168,7 @@ export default class Model {
 
     // requests
 
-
-    async save() {
+    async save () {
         this.queryBuilder.awaitType = QUERY_AWAIT_SINGLE;
         if (this.hasOwnProperty('id')) {
             return this.update();
@@ -182,48 +177,48 @@ export default class Model {
         return this.create();
     }
 
-    async create() {
+    async create () {
         this.queryBuilder.awaitType = QUERY_AWAIT_SINGLE;
 
         const url = this.queryBuilder.buildUrl(this.namespace);
         let response = await this.request(url, {
             method: this.REST_CONFIG.http_methods.create,
-            body: JSON.stringify(this.toJson()),
+            body: JSON.stringify(this.toJson())
         });
 
         return this.respond(response.data);
     }
 
-    async update() {
+    async update () {
         this.queryBuilder.awaitType = QUERY_AWAIT_SINGLE;
 
         const url = this.queryBuilder.buildUrl(this.namespace, this.id);
         let response = await this.request(url, {
             method: this.REST_CONFIG.http_methods.update,
-            body: JSON.stringify(this.toJson()),
+            body: JSON.stringify(this.toJson())
         });
 
         return this.respond(response.data);
     }
 
-    async delete() {
+    async delete () {
         this.queryBuilder.awaitType = QUERY_AWAIT_SINGLE;
 
         const url = this.queryBuilder.buildUrl(this.namespace, this.id);
         let response = this.request(url, {
-            method: this.REST_CONFIG.http_methods.delete,
+            method: this.REST_CONFIG.http_methods.delete
         });
 
         return this.respond(response.data);
     }
 
-    async attach(model, body = null, sync = false) {
+    async attach (model, body = null, sync = false) {
         this.queryBuilder.awaitType = QUERY_AWAIT_SINGLE;
 
         this.relationOfModel(this);
         const url = this.queryBuilder.buildUrl(model.namespace, model.id);
         const queryConfig = {
-            method: sync ? this.REST_CONFIG.http_methods.update : this.REST_CONFIG.http_methods.create,
+            method: sync ? this.REST_CONFIG.http_methods.update : this.REST_CONFIG.http_methods.create
         };
 
         if (body) {
@@ -235,7 +230,7 @@ export default class Model {
         return this.respond(response.data);
     }
 
-    async sync(model, data, detaching = true) {
+    async sync (model, data, detaching = true) {
         this.queryBuilder.awaitType = QUERY_AWAIT_SINGLE;
         if (!detaching) {
             this.queryBuilder.addCustomParameter(this.REST_CONFIG.request_keywords.sync_detaching, detaching);
@@ -243,14 +238,14 @@ export default class Model {
         return this.attach(model, data, true);
     }
 
-    async detach(model) {
+    async detach (model) {
         this.queryBuilder.awaitType = QUERY_AWAIT_SINGLE;
 
         this.relationOfModel(this);
 
         const url = this.queryBuilder.buildUrl(model.namespace, model.id);
         let response = await this.request(url, {
-            method: this.REST_CONFIG.http_methods.delete,
+            method: this.REST_CONFIG.http_methods.delete
         });
 
         return this.respond(response.data);
@@ -263,27 +258,23 @@ export default class Model {
      * @param json the json received
      * @returns the computed json
      */
-    computeJsonBeforeParsing(json) {
+    computeJsonBeforeParsing (json) {
         return json;
     }
 
     // lib
 
-    selfValidate() {
+    selfValidate () {
         const name = this.getNamespace();
 
         if (!name || !_.isString(name) || name.length === 0) {
             throw new Error(
-                `LRA Query Builder: Resource name not defined in ${this.constructor.name} model. Implement resourceName method in the ${this.constructor.name} model to resolve this error.`);
+                `LRA Query Builder: Namespace not defined in ${this.constructor.name} model. Implement getNamespace() method in the ${this.constructor.name} model to resolve this error.`);
         }
     }
 }
 
-
-const baseUrl = window.location ? window.location.href : "http://myserver.ext"
 Model.prototype.REST_CONFIG = {
-    base_url: baseUrl + '/api',
-
     default_temporal_field: 'created_at',
 
     cross_origin: false,
@@ -292,7 +283,7 @@ Model.prototype.REST_CONFIG = {
         get: 'GET',
         create: 'POST',
         update: 'PUT',
-        delete: 'DELETE',
+        delete: 'DELETE'
     },
 
     request_keywords: {
@@ -308,5 +299,5 @@ Model.prototype.REST_CONFIG = {
         distinct: 'selectDistinct',
         get_all: 'all',
         sync_without_detaching: 'sync_without_detaching'
-    },
+    }
 };
